0a13feb8ad0a68ef18bedf1564faa0da03ec6534
FANY HANDRO <119688317+fanhan20@users.noreply.github.com>
2024-05-23 19:52:57 +0700

    Update near.sh

--- near.sh
+++ near.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://near.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://near.lava.build/lava-referer-ac63f0fd-e5bd-4746-844a-db4351261e4f/"
 
 OUTPUT_DIR="logs"
 
